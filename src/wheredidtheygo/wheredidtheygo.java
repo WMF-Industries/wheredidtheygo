15373f860972412a0a1a8dc667d9dfa107e4d2cb
WMF <yourbestfriend1118@gmail.com>
2024-02-08 17:22:43 +0100

    clientside moment

--- src/wheredidtheygo/wheredidtheygo.java
+++ src/wheredidtheygo/wheredidtheygo.java
@@ -10,36 +10,36 @@ import mindustry.world.blocks.units.Reconstructor;
 import mindustry.world.blocks.units.UnitFactory;
 
 public class wheredidtheygo extends Mod{
-    public static Timer.Task updateRunner;
     public wheredidtheygo(){
         Log.info("Enemy units won't exist no more!");
-        Events.on(EventType.WorldLoadEvent.class, e ->{
-           updateRunner = Timer.schedule(wheredidtheygo::updateUnits, 0, 1f);
-           Groups.build.each(b -> {
-               if(b.team() != Vars.state.rules.defaultTeam
-               && b instanceof UnitFactory.UnitFactoryBuild
-               || b instanceof Reconstructor.ReconstructorBuild)
-                   b.enabled = false;
-           });
 
-           if(Vars.state.rules.winWave > 0 && Vars.state.isCampaign()){
-                Vars.state.wave = Vars.state.rules.winWave;
-           }else{
-               Vars.state.rules.waveSending = Vars.state.rules.waves = Vars.state.rules.waveTimer = false;
-           }
+        Events.on(EventType.ClientLoadEvent.class, e -> {
+            Timer.schedule(() -> {
+                if(!Vars.state.isGame() || Vars.net.client()) return;
+
+                Groups.unit.each(u -> {
+                    if(u.team() != Vars.state.rules.defaultTeam
+                            && !Vars.state.rules.pvp && !u.isPlayer())
+                        u.kill();
+                });
+            }, 0, 1f);
         });
-    }
 
-    private static void updateUnits(){
-        if(!Vars.state.isGame()){
-            updateRunner.cancel();
-            return;
-        }
+        Events.on(EventType.WorldLoadEvent.class, e -> {
+            if(Vars.net.client()) return;
 
-        Groups.unit.each(u -> {
-            if(u.team() != Vars.state.rules.defaultTeam
-            && !Vars.state.rules.pvp && !u.isPlayer())
-                u.kill();
+            Groups.build.each(b -> {
+                if(b.team() != Vars.state.rules.defaultTeam
+                && b instanceof UnitFactory.UnitFactoryBuild
+                || b instanceof Reconstructor.ReconstructorBuild)
+                    b.enabled = false;
+            });
+
+            if(Vars.state.rules.winWave > 0 && Vars.state.isCampaign()){
+                Vars.state.wave = Vars.state.rules.winWave;
+            }else{
+                Vars.state.rules.waveSending = Vars.state.rules.waves = Vars.state.rules.waveTimer = false;
+            }
         });
     }
 }
